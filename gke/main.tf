c13d0e7c64e7922353b81a8eb112f1d397b36a9b
Avik Bhattacharyya <Kambui.avik@gmail.com>
2025-07-26 00:52:32 +1000

    Reduce initial node count to 1 for GKE cluster and node pool

--- gke/main.tf
+++ gke/main.tf
@@ -17,7 +17,7 @@ resource "google_container_cluster" "primary" {
   name               = "my-preemptible-gke-cluster"
   location           = var.region
   remove_default_node_pool = true
-  initial_node_count = 2
+  initial_node_count = 1
   deletion_protection = false
   networking_mode          = "VPC_NATIVE"
   ip_allocation_policy {}
@@ -27,7 +27,7 @@ resource "google_container_node_pool" "preemptible_nodes" {
   name       = "preemptible-node-pool"
   location   = var.region
   cluster    = google_container_cluster.primary.name
-  node_count = 2
+  node_count = 1
 
   node_config {
     machine_type = "e2-medium" # Cheap & balanced
